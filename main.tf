95cadc3addac380fc6e0f356807eb8e586b7f826
Tom Harris <76956047+thomas-tank-corp@users.noreply.github.com>
2023-04-21 15:53:24 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.33.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.gcp_project_id
+}
+
+
+resource "google_container_cluster" "gke" {
+  name               = var.cluster_name
+  location           = "europe-west2-a"
+  initial_node_count = 1
+}
+
+variable "cluster_name" {}
+
+output "gke_cluster_id" {
+    value = google_container_cluster.gke.id
+}
+
+output "gke_cluster_master_endpoint" {
+    value = google_container_cluster.gke.endpoint
+}
