551d2c2e6f1ad4eee37fd21437623ce55926ed15
Tom Harris <76956047+thomas-tank-corp@users.noreply.github.com>
2023-11-22 13:33:37 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,14 +13,11 @@ resource "random_pet" "name" {}
 
 
 resource "google_container_cluster" "gke" {
-  name               = var.gke_cluster_name
+  name               = "gke-dev-${random_pet.name.id}"
   location           = "europe-west2-a"
   initial_node_count = 1
 }
 
-variable "gke_cluster_name" {
-  default = "gke-dev-${random_pet.name.id}"
-}
 
 
 output "gke_cluster_id" {
